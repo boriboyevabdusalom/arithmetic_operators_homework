6b55cb3e871d87a7dcb246aae87e72019ae9758c
abdusalom <boriboyevabdusalom@gmail.com>
2023-10-20 09:45:01 +0500

    clon

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -1,11 +1,11 @@
 #Create a variable called 'number' and assign it the three-digit number.
-
+number = 678
 #Find the 'number' first digit and assign to x1.
-
+n1 =number%10
 #Find the 'number' second digit and assign to x2.
-
+n2 = number//10%10
 #Find the 'number' third digit and assign to x3.
-
+n3 = number//100
 #Create a variable called 'answer' and assign it the sum of the three digits x1, x2, x3.
-
+print( n1 + n2 + n3)
 #Print the value of the 'answer.
